dcae068f01381a7338cc163cc211bdf17e5d1ac8
Venkatesa Balaji K <venkatesabalajik@outlook.com>
2025-02-15 20:19:48 +0530

    fix: fix provided for log not getting generated

--- src/main/java/com/api/services/BaseService.java
+++ src/main/java/com/api/services/BaseService.java
@@ -1,5 +1,7 @@
 package com.api.services;
 
+import com.api.filters.LoggingFilter;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -8,6 +10,10 @@ import io.restassured.specification.RequestSpecification;
 public class BaseService {
 	private static final String BASE_URI = "http://64.227.160.186:8080";
 	protected RequestSpecification requestSpec;
+	
+	static {
+		RestAssured.filters(new LoggingFilter());
+	}
 
 	public BaseService() {
 		this.requestSpec = RestAssured.given().baseUri(BASE_URI);
@@ -16,5 +22,5 @@ public class BaseService {
 	public Response postRequest(Object payload, String endPoint) {
 		return requestSpec.contentType(ContentType.JSON).body(payload).post(BASE_URI + endPoint);
 	}
-	
+
 }
--- src/test/java/com/api/tests/AuthenticationTests.java
+++ src/test/java/com/api/tests/AuthenticationTests.java
@@ -1,21 +1,34 @@
 package com.api.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import com.api.request.models.LoginRequest;
+import com.api.request.models.LoginRequestModel;
+import com.api.request.models.SignupRequestModel;
+import com.api.response.models.LoginResponse;
 import com.api.services.AuthenticationService;
 
 import io.restassured.response.Response;
 
+@Listeners(com.api.listerners.TestListener.class)
 public class AuthenticationTests {
 
 	@Test(description = "Login test")
 	public void loginRequestTest() {
 		AuthenticationService authenticationService = new AuthenticationService();
-		LoginRequest loginDetails = new LoginRequest("Bala1234", "Bala1234");
-
+		LoginRequestModel loginDetails = new LoginRequestModel("Bala1234", "Bala1234");
 		Response response = authenticationService.loginRequest(loginDetails);
-		System.out.println(response.asPrettyString());
+		LoginResponse loginResponse = response.as(LoginResponse.class);
+		Assert.assertTrue(loginResponse.getToken() != null);
+	}
+
+	@Test(description = "Sign up")
+	public void signup() {
+		AuthenticationService authenticationService = new AuthenticationService();
+		SignupRequestModel SignupDetails = new SignupRequestModel.Builder().username("Bala1234").password("Bala1234")
+				.email("bala@gmail.com").firstName("Bala").lastName("Kandavel").mobileNumber(1244545645l).buildSignup();
+		authenticationService.signupRequest(SignupDetails);
 	}
 
 }
